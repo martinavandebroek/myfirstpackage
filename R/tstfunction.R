20dc068aa877e0c889a9623b1ac53a005fdd3035
martinavandebroek <49563137+martinavandebroek@users.noreply.github.com>
2021-01-21 14:35:08 +0100

    exporteren?

---
+++ R/tstfunction.R
@@ -0,0 +1,8 @@
+#'the function tstpkg
+#'
+#'@import  lm from stats
+# moet ook nog eens in de description file vermeld worden bij Imports:...
+#'@export
+tstpkg = function(rept){ x = rep(rept,rept) }
+
+
